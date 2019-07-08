fe26909423cdc5535696acf54705ae2c7d3da643
Steve Uffelman <steve@uffelman.co>
2019-07-08 11:59:59 -0700

    adds mac target

---
+++ TableSchemer-macOS/TableSchemer_macOS.h
@@ -0,0 +1,19 @@
+//
+//  TableSchemer_macOS.h
+//  TableSchemer-macOS
+//
+//  Created by Stephen Uffelman on 7/8/19.
+//  Copyright © 2019 Weebly. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TableSchemer_macOS.
+FOUNDATION_EXPORT double TableSchemer_macOSVersionNumber;
+
+//! Project version string for TableSchemer_macOS.
+FOUNDATION_EXPORT const unsigned char TableSchemer_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableSchemer_macOS/PublicHeader.h>
+
+
